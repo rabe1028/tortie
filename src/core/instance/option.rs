a751b25bacf3926777f4e6e051a22db4aaf1c47e
Ryo Abe <30817722+rabe1028@users.noreply.github.com>
2022-01-14 14:54:00 +0900

    remove unused code

--- src/core/instance/option.rs
+++ src/core/instance/option.rs
@@ -1,15 +1,5 @@
 use crate::core::invariant::*;
 
-// impl Invariant for OptionHigherKind {
-//     fn imap<A, B>(
-//         fa: Self::F<A>,
-//         f: impl Fn(A) -> B,
-//         _g: impl Fn(B) -> A,
-//     ) -> Self::F<B> {
-//         fa.map(f)
-//     }
-// }
-
 impl<A> Invariant<'_> for Option<A> {
     type Domain = A;
     type InvariantF<B> = Option<B>;
--- src/core/instance/result.rs
+++ src/core/instance/result.rs
@@ -1,15 +1,5 @@
 use crate::core::invariant::*;
 
-// impl<E> Invariant for ResultHigherKind<E> {
-//     fn imap<A, B>(
-//         fa: Self::F<A>,
-//         f: impl Fn(A) -> B,
-//         _g: impl Fn(B) -> A,
-//     ) -> Self::F<B> {
-//         fa.map(f)
-//     }
-// }
-
 impl<A, E> Invariant<'_> for Result<A, E> {
     type Domain = A;
     type InvariantF<B> = Result<B, E>;
